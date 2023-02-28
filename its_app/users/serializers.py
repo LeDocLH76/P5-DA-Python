ad54bfcd930846fabd6f53cfb7133b3e88e43cbc
LeDoc <ledoclh76fr@gmail.com>
2023-02-28 15:45:48 +0100

    Rebuild database. Add secure password validation.

--- its_app/users/serializers.py
+++ its_app/users/serializers.py
@@ -1,7 +1,9 @@
-# from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from django.contrib.auth import password_validation
+from django.core.exceptions import ValidationError
+
 from rest_framework import serializers
 from rest_framework.authentication import authenticate
-from django.contrib.auth import get_user_model
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,6 +17,27 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ['username', 'password', 'email', 'first_name', 'last_name']
+
+    def validate_password(self, data):
+
+        user = get_user_model()(
+            username=self.initial_data['username'],
+            password=self.initial_data['password'],
+            email=self.initial_data['email'],
+            first_name=self.initial_data['first_name'],
+            last_name=self.initial_data['last_name'],
+        )
+        errors = dict()
+        try:
+            password_validation.validate_password(
+                password=user.password,
+                user=user
+            )
+        except ValidationError as e:
+            errors['password'] = list(e.messages)
+        if errors:
+            raise serializers.ValidationError(errors)
+        return super(RegisterSerializer, self).validate(data)
 
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
