a6ee0dba9df0aac00879eb493e1eb95a66fb6d29
LeDoc <ledoclh76fr@gmail.com>
2023-02-28 13:14:50 +0100

    Flush database. Delete migrations. Change database from postgres to sqlite3.

--- config/settings.py
+++ config/settings.py
@@ -97,23 +97,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'P5DAP',
-        'USER': 'postgres',
-        'PASSWORD': os.getenv('POSTGRESPASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'P5DAP',
+#         'USER': 'postgres',
+#         'PASSWORD': os.getenv('POSTGRESPASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
