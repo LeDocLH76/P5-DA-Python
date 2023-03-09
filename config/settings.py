4ebf58b4dc69e75092a669b4d5970a4aa12eb762
LeDoc <ledoclh76fr@gmail.com>
2023-03-09 10:46:01 +0100

    Change refresh token lifetime

--- config/settings.py
+++ config/settings.py
@@ -58,7 +58,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=120),
 }
 
 
